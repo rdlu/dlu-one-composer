218451f0304da1e11ba544aad755c73042d94b85
Rodrigo Dlugokenski <r@dlu.one>
2019-09-12 23:04:03 -0300

    Moving compose files to single directory

--- rund.fish
+++ rund.fish
@@ -1,4 +1,5 @@
 #! /usr/bin/fish
 set PROJECT $argv[1]
 sudo systemctl start docker
-docker-compose -f $PROJECT.docker-compose.yml -p $PROJECT up -d 
+docker-compose -f compose/traefik.docker-compose.yml -p traefik up -d 
+docker-compose -f compose/$PROJECT.docker-compose.yml -p $PROJECT up -d 
--- stopd.fish
+++ stopd.fish
@@ -1,4 +1,4 @@
 #! /usr/bin/fish
 set PROJECT $argv[1]
 sudo systemctl start docker
-docker-compose -f $PROJECT.docker-compose.yml -p $PROJECT down 
+docker-compose -f compose/$PROJECT.docker-compose.yml -p $PROJECT down 
