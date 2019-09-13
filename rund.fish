a73cf1ea97142e214f1d041616f7b2699340d8ec
Rodrigo Dlugokenski <r@dlu.one>
2019-09-13 00:25:36 -0300

    Run defaults

--- rund.fish
+++ rund.fish
@@ -1,5 +1,9 @@
 #! /usr/bin/fish
+
+set -q argv[1]; or set argv[1] traefik
 set PROJECT $argv[1]
 sudo systemctl start docker
-docker-compose -f compose/traefik.docker-compose.yml -p traefik up -d 
-docker-compose -f compose/$PROJECT.docker-compose.yml -p $PROJECT up -d 
+docker-compose -f compose/traefik.docker-compose.yml -p traefik up -d
+if test $PROJECT != 'traefik'
+    docker-compose -f compose/$PROJECT.docker-compose.yml -p $PROJECT up -d
+end
