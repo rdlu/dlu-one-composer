91527cc80e27f6bbb2ec5b437d4d8da665aaa800
Rodrigo Dlugokenski <rodrigo@qcx.com.br>
2019-09-13 15:07:35 -0300

    Stopd default traefik

--- stopd.fish
+++ stopd.fish
@@ -1,4 +1,5 @@
 #! /usr/bin/fish
+set -q argv[1]; or set argv[1] traefik
 set PROJECT $argv[1]
 sudo systemctl start docker
-docker-compose -f compose/$PROJECT.docker-compose.yml -p $PROJECT down 
+docker-compose -f compose/$PROJECT.docker-compose.yml -p $PROJECT down
