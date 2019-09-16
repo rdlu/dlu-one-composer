802878881d3352ae738f8f3c19961efbfc9cc096
Rodrigo Dlugokenski <rodrigo@qcx.com.br>
2019-09-16 13:56:22 -0300

    more colors

--- rund.fish
+++ rund.fish
@@ -10,5 +10,5 @@ set_color cyan; echo "Making sure traefik is running..."
 set_color grey; docker-compose -f compose/traefik.docker-compose.yml -p traefik up -d
 if test $PROJECT != 'traefik'
     set_color yellow; echo "Trying to run your project compose..."
-    set_color green; docker-compose -f compose/$PROJECT.docker-compose.yml -p $PROJECT up -d
+    set_color reset; docker-compose -f compose/$PROJECT.docker-compose.yml -p $PROJECT up -d
 end
