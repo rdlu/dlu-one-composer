3a9b9c319b1ca194beff201de8fd9db662dab481
Rodrigo Dlugokenski <rodrigo@qcx.com.br>
2019-09-16 10:13:52 -0300

    Colors for run + log default

--- logd.fish
+++ logd.fish
@@ -1,3 +1,5 @@
 #! /usr/bin/fish
+set -q argv[1]; or set argv[1] traefik
 set PROJECT $argv[1]
+
 docker-compose -f compose/$PROJECT.docker-compose.yml -p $PROJECT logs -f
--- rund.fish
+++ rund.fish
@@ -6,7 +6,9 @@ set UID (id -u (whoami))
 set GID (id -g (whoami))
 
 sudo systemctl start docker
-docker-compose -f compose/traefik.docker-compose.yml -p traefik up -d
+set_color cyan; echo "Making sure traefik is running..."
+set_color grey; docker-compose -f compose/traefik.docker-compose.yml -p traefik up -d
 if test $PROJECT != 'traefik'
-    docker-compose -f compose/$PROJECT.docker-compose.yml -p $PROJECT up -d
+    set_color yellow; echo "Trying to run your project compose..."
+    set_color green; docker-compose -f compose/$PROJECT.docker-compose.yml -p $PROJECT up -d
 end
