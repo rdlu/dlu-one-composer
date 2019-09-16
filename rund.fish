2b95139dd2acc896457044918af2d5e5c9591cfe
Rodrigo Dlugokenski <rodrigo@qcx.com.br>
2019-09-16 09:51:04 -0300

    setting GID and UID for come compose files that requires it

--- rund.fish
+++ rund.fish
@@ -2,6 +2,9 @@
 
 set -q argv[1]; or set argv[1] traefik
 set PROJECT $argv[1]
+set UID (id -u (whoami))
+set GID (id -g (whoami))
+
 sudo systemctl start docker
 docker-compose -f compose/traefik.docker-compose.yml -p traefik up -d
 if test $PROJECT != 'traefik'
