852f2303bd329d0e1a270321b9129df902f6aec0
Rodrigo Dlugokenski <r@dlu.one>
2020-04-09 16:18:07 -0300

    Improvements on self ca generation

--- certs/self-certificate.bash
+++ certs/self-certificate.bash
@@ -1,30 +1,35 @@
 #! /bin/bash
-SERVER="${SERVER:-lan}"
+SERVER="${1:-lan}"
 CORPORATION=DluOneComposer
 GROUP=DluOneComposer
 CITY=City
 STATE=State
 COUNTRY=BR
 
-# CERT_AUTH_PASS=`openssl rand -base64 32`
-# echo $CERT_AUTH_PASS > cert_auth_password
+if [ ! -f cert_auth_password ]; then
+  echo "Generating a password for CA..."
+  CERT_AUTH_PASS=`openssl rand -base64 32`
+  echo $CERT_AUTH_PASS > cert_auth_password
+fi
 CERT_AUTH_PASS=`cat cert_auth_password`
 
-# create the certificate authority
-# openssl \
-#   req \
-#   -subj "/CN=$SERVER.ca/OU=$GROUP/O=$CORPORATION/L=$CITY/ST=$STATE/C=$COUNTRY" \
-#   -new \
-#   -x509 \
-#   -passout pass:$CERT_AUTH_PASS \
-#   -keyout ca-cert.key \
-#   -out ca-cert.crt \
-#   -days 36500
+if [ ! -f ca-cert.key ]; then
+  echo "Creating a new certificate authority for $SERVER..."
+  openssl \
+    req \
+    -subj "/CN=$SERVER.ca/OU=$GROUP/O=$CORPORATION/L=$CITY/ST=$STATE/C=$COUNTRY" \
+    -new \
+    -x509 \
+    -passout pass:$CERT_AUTH_PASS \
+    -keyout ca-cert.key \
+    -out ca-cert.crt \
+    -days 1825
+fi
 
-# create client private key (used to decrypt the cert we get from the CA)
+echo "Creating a client private key (used to decrypt the cert we get from the CA)"
 openssl genrsa -out $SERVER.key
 
-# create the CSR(Certitificate Signing Request)
+echo "Creating the CSR (Certitificate Signing Request) for $SERVER"
 openssl \
   req \
   -new \
@@ -36,13 +41,13 @@ openssl \
   -key $SERVER.key \
   -out $SERVER.csr \
   -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS.1:$SERVER,DNS.2:*.ssl.$SERVER.dev,DNS.3:*.$SERVER")) \
-  -days 36500
+  -days 365
 
 # sign the certificate with the certificate authority
 openssl \
   x509 \
   -req \
-  -days 36500 \
+  -days 365 \
   -in $SERVER.csr \
   -CA ca-cert.crt \
   -CAkey ca-cert.key \
@@ -52,12 +57,21 @@ openssl \
   -extensions SAN \
   -passin pass:$CERT_AUTH_PASS
 
-# sudo trust anchor --store ca-cert.crt
 
+cat << EOF
+REMEMBER: Add the CA certificate to your target operating systems:
 
+# ArchLinux / Manjaro
 
-# Mac Add Cert
-# sudo security -v add-trusted-cert -r trustAsRoot -e hostnameMismatch -d -k /Library/Keychains/System.keychain "./server.crt"
+ sudo trust anchor --store ca-cert.crt
+
+# MacOS X
+
+ sudo security -v add-trusted-cert -r trustAsRoot -e hostnameMismatch -d -k /Library/Keychains/System.keychain "./ca-cert.crt"
+EOF
 
 # Mac OpenSSL config
 #cat /System/Library/OpenSSL/openssl.cnf
+
+cp -f ../configs/traefik/certificates.toml.default ../configs/traefik/certificates.toml
+sed -i "s/localhost/$SERVER/g" ../configs/traefik/certificates.toml
