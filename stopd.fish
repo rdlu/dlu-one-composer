ae8642c37d6c2455548e7dc16e71bd3ba610e614
Rodrigo Dlugokenski <r@dlu.one>
2020-04-09 15:47:48 -0300

    gid uid for stopd

--- stopd.fish
+++ stopd.fish
@@ -1,5 +1,7 @@
 #! /usr/bin/fish
 set -q argv[1]; or set argv[1] traefik
 set PROJECT $argv[1]
+set -x UID (id -u (whoami))
+set -x GID (id -g (whoami))
 sudo systemctl start docker
 docker-compose -f compose/$PROJECT.docker-compose.yml -p $PROJECT down
